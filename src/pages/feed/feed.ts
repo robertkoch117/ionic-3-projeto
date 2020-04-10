bb7fddc1fd850d7e0f24fa79fa6352d1a3b6457c
robertkoch117 <robertkoch117@gmail.com>
2020-04-10 10:32:41 -0300

    exibindo mensagem de carregando filmes

--- src/pages/feed/feed.ts
+++ src/pages/feed/feed.ts
@@ -1,5 +1,5 @@
 import { Component } from '@angular/core';
-import { IonicPage, NavController, NavParams } from 'ionic-angular';
+import { IonicPage, NavController, NavParams, LoadingController } from 'ionic-angular';
 import { MoovieProvider } from "../../providers/moovie/moovie";
 
 /**
@@ -32,26 +32,43 @@ export class FeedPage {
 
   public nome_usuario:string = "Robert Koch do Código";
   
+  public loader;
+  
   constructor(
     public navCtrl: NavController, 
     public navParams: NavParams,
-    private movieProvider: MoovieProvider) {
+    private movieProvider: MoovieProvider,
+    public loadingCtrl: LoadingController) {
+  }
+
+  abreCarregando() {
+    this.loader = this.loadingCtrl.create({
+      content: "Carregando filmes..."
+    });
+    this.loader.present();
+  }
+
+  fechaCarregando(){
+    this.loader.dismiss();
   }
 
   public somaDoisNumeros(num1:number, num2:number):void{
     //alert(num1+num2);
   }
 
-  ionViewDidLoad() {
+  ionViewDidEnter() {
+    this.abreCarregando();
     this.movieProvider.getLatestMovies().subscribe(
       data=>{
         const response = (data as any);
         //const objeto_retorno = JSON.parse(response); 
         this.lista_filmes= response.results;
         
+        this.fechaCarregando();
         console.log(response);
       },error => {
         console.log(error);
+        this.fechaCarregando();
       }
     )
   }
